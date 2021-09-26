a39a2f077467c4af0683214132c41145e3846b8f
hatso <mmorpg3636@naver.com>
2021-09-26 19:01:16 +0900

    WeBank linktest work 09/26

--- myapp/routes/test.js
+++ myapp/routes/test.js
@@ -5,5 +5,8 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('test', { title: 'Test' ,home:'WeBank'});
 });
+router.get('/test2', (req,res) => {
+  res.render('test', { title: 'Test2',home:'WeBank'})
+})
 
 module.exports = router;
