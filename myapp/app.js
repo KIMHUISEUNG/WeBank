6d05c10c83ce39d0cb0fe0cd734a08dbfc6186ce
hatso <mmorpg3636@naver.com>
2021-10-07 14:33:41 +0900

    index work 1 10/07

--- myapp/app.js
+++ myapp/app.js
@@ -22,8 +22,8 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use("/public", express.static(path.join(__dirname, '/public')));
-
+app.use(express.static(path.join(__dirname, '/public')));
+//app.use("/public/images/naver", express.static(path.join(__dirname, 'public')));
 //app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/', indexRouter);
